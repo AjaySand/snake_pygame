e8a7cd290f405f0d4a6217ed19bab988eb3b4b3b
Ajaydeep Sandhu <ajaysan.1996@gmail.com>
2023-10-04 01:03:21 +0200

    Initial commit

---
+++ snake_oop.py
@@ -0,0 +1,218 @@
+import math
+import random
+import pygame
+import tkinter as tk
+
+# constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+BACKGROUND_COLOR = (0, 0, 0)  # White
+ROWS = 20
+
+
+class Cube(object):
+    rows = ROWS
+    w = SCREEN_WIDTH
+
+    def __init__(self, start, dir_x=1, dir_y=0, color=(255, 0, 0)):
+        self.pos = start
+        self.dir_x = dir_x
+        self.dir_y = dir_y
+        self.color = color
+
+    def move(self, dir_x, dir_y):
+        self.dir_x = dir_x
+        self.dir_y = dir_y
+
+        self.pos = (self.pos[0] + self.dir_x, self.pos[1] + self.dir_y)
+
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]  # row
+        j = self.pos[1]  # column
+
+        pygame.draw.rect(
+            surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2)
+        )
+
+        if eyes:
+            centre = dis // 2
+            radius = 3
+            circle_middle = (i * dis + centre - radius, j * dis + 8)
+            circle_middle2 = (i * dis + dis - radius * 2, j * dis + 8)
+            pygame.draw.circle(surface, (0, 0, 0), circle_middle, radius)
+            pygame.draw.circle(surface, (0, 0, 0), circle_middle2, radius)
+
+
+class Snake(object):
+    body = []
+    turns = {}
+
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = Cube(pos)
+        self.body.append(self.head)
+        self.dir_x = 0
+        self.dir_y = 1
+
+    def move(self):
+        ## add pressed keys to the turns dictionary
+        new_dir_x = self.dir_x
+        new_dir_y = self.dir_y
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+
+            keys = pygame.key.get_pressed()
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    new_dir_x = -1
+                    new_dir_y = 0
+                elif keys[pygame.K_RIGHT]:
+                    new_dir_x = 1
+                    new_dir_y = 0
+                elif keys[pygame.K_UP]:
+                    new_dir_x = 0
+                    new_dir_y = -1
+                elif keys[pygame.K_DOWN]:
+                    new_dir_x = 0
+                    new_dir_y = 1
+
+        # check if dir changed
+        if (new_dir_x, new_dir_y) != (self.dir_x, self.dir_y):
+            self.dir_x = new_dir_x
+            self.dir_y = new_dir_y
+            self.turns[self.head.pos[:]] = [self.dir_x, self.dir_y]
+
+        ## Move the snake
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+
+            # if the position is in the turns dictionary
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+
+                # pop the last turn
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if c.dir_x == -1 and c.pos[0] <= 0:
+                    c.pos = (c.rows - 1, c.pos[1])
+                elif c.dir_x == 1 and c.pos[0] >= c.rows - 1:
+                    c.pos = (0, c.pos[1])
+                elif c.dir_y == 1 and c.pos[1] >= c.rows - 1:
+                    c.pos = (c.pos[0], 0)
+                elif c.dir_y == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.rows - 1)
+                else:
+                    c.move(c.dir_x, c.dir_y)
+
+        return True
+
+    def reset(self, pos):
+        self.head = Cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dir_x = 0
+        self.dir_y = 1
+
+    def add_cube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dir_x, tail.dir_y
+
+        if dx == 1 and dy == 0:
+            self.body.append(Cube((tail.pos[0] - 1, tail.pos[1]), dx, dy))
+        elif dx == -1 and dy == 0:
+            self.body.append(Cube((tail.pos[0] + 1, tail.pos[1]), dx, dy))
+        elif dx == 0 and dy == 1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1] - 1), dx, dy))
+        elif dx == 0 and dy == -1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1] + 1), dx, dy))
+
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+
+
+def draw_grid(rows, width, surface):
+    size_between = width // rows
+    x = 0
+    y = 0
+
+    for i in range(rows):
+        x += size_between
+        y += size_between
+
+        # Draw horizontal lines
+        pygame.draw.line(surface, (100, 100, 100), (x, 0), (x, width))
+
+        # Draw vertical lines
+        pygame.draw.line(surface, (100, 100, 100), (0, y), (width, y))
+
+
+def redraw_window(surface):
+    global snake, snack
+    surface.fill(BACKGROUND_COLOR)
+    snake.draw(surface)
+    snack.draw(surface)
+    draw_grid(ROWS, SCREEN_WIDTH, surface)
+
+
+def random_snack(rows, snake):
+    positions = snake.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+
+    return (x, y)
+
+
+def main():
+    global snake, snack
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Snake Game")
+
+    clock = pygame.time.Clock()
+    snake = Snake((255, 0, 0), (10, 10))
+
+    snack = Cube(random_snack(ROWS, snake), color=(0, 255, 0))
+
+    running = True
+    while running:
+        pygame.time.delay(120)
+        clock.tick(60)
+
+        # Update game logic here
+        running = snake.move()
+
+        if snake.body[0].pos == snack.pos:
+            snake.add_cube()
+            snack = Cube(random_snack(ROWS, snake), color=(0, 255, 0))
+
+        for x in range(len(snake.body)):
+            if snake.body[x].pos in list(map(lambda z: z.pos, snake.body[x + 1 :])):
+                print("Score: ", len(snake.body))
+                snake.reset((10, 10))
+                break
+
+        # Draw game elements here
+        redraw_window(screen)
+
+        # Update the display
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
