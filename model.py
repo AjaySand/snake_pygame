95451395809015b0d5f0f62f6a28a18ee50bfa1d
Ajaydeep Sandhu <ajaysan.1996@gmail.com>
2023-10-07 05:20:49 +0200

    added another hidden layer to nn

--- model.py
+++ model.py
@@ -6,15 +6,21 @@ import os
 
 
 class Linear_QNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, hidden_size2, output_size):
         super().__init__()
 
+        cuda = torch.cuda.is_available()
+        print("CUDA is available:", cuda)
+        self.device = torch.device("cuda:0" if cuda else "cpu")
+
         self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size2)
+        self.linear3 = nn.Linear(hidden_size2, output_size)
 
     def forward(self, x):
-        x = Fq.relu(self.linear1(x))
-        x = self.linear2(x)
+        x = Fq.relu6(self.linear1(x))
+        x = Fq.relu6(self.linear2(x))
+        x = self.linear3(x)
         return x
 
     def save(self, file_name="model.pth"):
